ca9faa23781defe912a1e3eb3c3aa370790ff627
敦史 <midori06180410@gmil.com>
2021-01-10 19:52:59 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
