7c3364c9192bd932b7675b120e8622b9b9118462
敦史 <midori06180410@gmil.com>
2021-01-10 19:25:48 +0900

    ユーザー編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:password])
   end
 
 end
